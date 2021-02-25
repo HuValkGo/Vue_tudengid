e8ba5299b6bab638b32a80c8dd24de55ec4bd003
huvalk <huvalk@ttu.ee>
2021-02-25 10:48:24 +0200

    commit

--- src/components/AddStudentForm.vue
+++ src/components/AddStudentForm.vue
@@ -50,7 +50,7 @@
             v-model="student.sex"
             class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
           >
-            <option disabled value="">Vali üks</option>
+            <option disabled value="">Vali palun üks</option>
             <option value="Mees">Mees</option>
             <option value="Naine">Naine</option>
           </select>
